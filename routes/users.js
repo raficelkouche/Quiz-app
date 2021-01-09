6dbb763e5112842e61cefd71e02e7edcc4586734
Tammy Tran <tammy.n.tran@gmail.com>
2021-01-09 20:46:43 +0000

    add requirements as comments for users routes

--- routes/users.js
+++ routes/users.js
@@ -9,6 +9,8 @@ const express = require('express');
 const router  = express.Router();
 
 module.exports = (db) => {
+
+  // to see current users database - for dev
   router.get("/", (req, res) => {
     db.query(`SELECT * FROM users;`)
       .then(data => {
@@ -21,5 +23,26 @@ module.exports = (db) => {
           .json({ error: err.message });
       });
   });
+
+  // need the following routes
+
+  // users POST - create new account
+
+  // users/new GET - registration page
+
+  // users/:user_id  GET - get user page with their attempt history
+
+  // users/:user_id DELETE - delete user
+
+  // users/:user_id PUT - edit user
+
+  // users/:user_id/edit GET - get user edit page
+
+  // users/:users_id/quizzes GET - get all quizzes by the user
+
+  // users/:users_id/quizzes/:quiz_id GET - view quiz with creator access
+
+
+
   return router;
 };
