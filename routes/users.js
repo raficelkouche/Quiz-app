7448760a5986afbe39770676fab6d9fe0ed3f820
Tammy Tran <tammy.n.tran@gmail.com>
2021-01-09 23:27:57 +0000

    add the following routes: users/:user_id/edit GET, users/:user_id/quizzes GET, users/:user_id/quizzes/:quiz_id GET

--- routes/users.js
+++ routes/users.js
@@ -92,27 +92,68 @@ module.exports = (db) => {
   router.put('/:user_id', (req, res) => {
     // check if correct user is logged in to edit
     if (db.getUserWithID(user_id) === req.session) {
+      // need to save the form data as new info (will need to review
+      // the req.body data structure)
+      const new_info = req.body
       // editUser helper should take two parameters, user_id and new info
       // it then updates the db
-      db.editUser(user_id, [new_nfo])
+      db.editUser(user_id, [new_info])
       .then ( newUser => {
         // with the new user info returned, cookie does not need to be updated
         // since user_id should remain the same
         // re-render the user_id page with new info
         res.redirect(`/${req.params.user_id}`)
-
-
       })
     }
   })
 
   // users/:user_id/edit GET - get user edit page
+  router.get('/:user_id/edit', (req, res) => {
+    // this line is for the nav bar with user logged in
+    const templateVars = {user_id: req.session.user_id};
+    // render the page with the fields for edit
+    res.render('user_edit', templateVars)
+  })
 
-  // users/:users_id/quizzes GET - get all quizzes by the user
+  // users/:user_id/quizzes GET - get all quizzes by the user
+  router.get('/:user_id/quizzes', (req, res) => {
+    db.getQuizzesStats(user_id)
+    .then (quizzes => {
+      // quizzes should be all the quiz that belogs to the user_id
+      // depending on the structure, may have to manipulate to work
+
+      const templateVars = {user_id: req.session.user_id, quizzes: quizzes};
+
+      // render page that will show all quizzes into table
+      res.render("user_quizzes", templateVars);
+    })
+    .catch(e => res.send(e));
+  })
 
   // users/:users_id/quizzes/:quiz_id GET - view quiz with creator access
-
-
+  router.get('/:user_id/quizzes/:quiz_id', (req, res) => {
+    // get the quiz info
+    db.getQuizWithId(quiz_id)
+    .then (quiz => {
+      // check if the cookie user = quiz user id (creator looking at the quiz)
+      if (quiz.owner_id === req.session.user_id) {
+        // quiz info here to render
+        const templateVars = {
+          title: quiz.title,
+          photo_url: quiz.photo_url,
+          quiz_url: quiz.quiz_url,
+          category: quiz.category
+        }
+        // need the creator name
+        db.getUserWithID(quiz.owner_id)
+        .then (creator => {
+          templateVars.creator = creator.name
+          // page rendering for quiz to be viewed by creator
+          res.render('user_quiz', templateVars)
+        })
+      }
+    })
+  })
 
   return router;
 };
