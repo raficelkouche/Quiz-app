1d5e79931d7a9dbecc16400421cd8bc36edb7f1b
Rafic El Kouche <raficelkouche@hotmail.com>
2021-01-10 21:01:30 -0500

    add /quizzes/:quiz_URL GET, /quizzes/:quiz_ID PUT

--- routes/quizzes.js
+++ routes/quizzes.js
@@ -26,18 +26,108 @@ module.exports = (db) => {
       });
   });
   // inserts a new quiz and redirects to "my quizzes" page
+  // test once db helper methods are available
   router.post("/", (req, res) => {
     const userID = req.session.userID;
     if (userID) {
       //all the information will be passed from the html form except the userID and quiz_url
       addQuiz({ userID, title, description, visibility, photo_url, quiz_url, category })
+      //redirect to the owner's page showing all the created quizzes
       res.render("user_quizzes", {userID});
     } else {
       //redirecting is not necessary since this case won't occur in a browser
       res.status(403).send('Forbidden');
     }
-  })
+  });
 
+  router.get("/:quiz_URL", (req, res) => {
+    //quiz can be taken by anyone as long as they have the URL
+    //call getQuizWithURL(quizURL) here, should return quiz details, questions and answers
+    const queryString = `SELECT * FROM quizzes WHERE quiz_url = '${req.params.quiz_URL}';`
+    db.query(queryString)
+      .then(result => {
+        res.render("../testFiles/take_quiz", {quizData: result.rows})
+      })
+      .catch(err => {
+        console.log("query failed: ", err.stack);
+        res.statusCode = 404
+        res.render("error", {error: "couldn't retrieve quiz"});
+      })
+  });
+  /*
+  this will have the form, pre-filled with all the current Q&A and can be editted
+  submitting this form will send a PUT request to /quizzes/:quiz_id
+  */
+  router.get("/:quiz_id/edit", (req, res) => {
+    const userID = req.session.userID
+    /*check if this user is the owner of the quiz:
+    call checkQuizOwner(userID, quizID)*/
+    const queryString = `
+        SELECT quizzes.owner_id, quizzes.id
+        FROM quizzes
+        JOIN users ON quizzes.owner_id = users.id
+        WHERE quizzes.owner_id = ${userID} AND quizzes.id = ${req.params.quiz_id};
+        `
+    db.query(queryString)
+      .then(result => {
+        if (result.length > 0) {
+          //render the edit page now
+          //query the db to get the current questions and answers of that quiz
+          //call getQuizWithID() here
+          const queryString = `
+            SELECT *
+            FROM quizzes
+            WHERE id = ${req.params.quiz_id};
+          `
+          db.query(queryString)
+            .then(result => {
+              const quizData = result.rows
+              res.render("edit_quiz", { userID, quizData });
+            })
+            .catch(err => {
+              console.log("query error", err.stack);
+              res.statusCode = 404;
+              res.render("error", {error: "Operation Failed!"});
+            });
+        } else {
+          res.statusCode = 403;
+          res.render("error", { error: "Access Denied!" });
+        }
+      })
+      .catch(err => {
+        console.log("query error", err.stack);
+        res.statusCode = 404;
+        res.render("error", { error: "Operation Failed!" });
+      });
+  });
+
+  //could be either quiz_id or quiz_URL for consistency ...to be discussed
+  router.put("/:quiz_id", (req, res) => {
+      const userID = req.session.userID
+      /*check if this user is the owner of the quiz:
+      call checkQuizOwner(userID, quizID)*/
+      const queryString = `
+        SELECT quizzes.owner_id, quizzes.id
+        FROM quizzes
+        JOIN users ON quizzes.owner_id = users.id
+        WHERE quizzes.owner_id = ${userID} AND quizzes.id = ${req.params.quiz_id};
+        `
+      db.query(queryString)
+        .then(result => {
+          if (result.length > 0) {
+            //call the alter query now editQuiz(quizInfo)
+
+          } else {
+            res.statusCode = 403;
+            res.render("error", {error: "Access Denied!"});
+          }
+        })
+        .catch (err => {
+          console.log("query error", err.stack);
+          res.statusCode = 404;
+          res.render("error", {error: "Operation Failed!"});
+        });
+  });
 
   return router;
 };
