f79dcb68d34059ae4432414395f032b8d4bfd900
Wilkie Wai-Kit Wong <feanor4d@gmail.com>
2021-01-12 14:57:34 +0000

    add small amount of seeds for testing

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,22 +1,28 @@
 -- Users table seeds here (Example)
 
-    INSERT INTO users (
-    name, email, password)
-    VALUES (
-    'Devin Sanders', 'tristanjacobs@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
-    INSERT INTO users (
-    name, email, password)
-    VALUES (
-    'Iva Harrison', 'allisonjackson@mail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
-    INSERT INTO users (
-    name, email, password)
-    VALUES (
-    'Lloyd Jefferson', 'asherpoole@gmx.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
-    INSERT INTO users (
-    name, email, password)
-    VALUES (
-    'Dale Coleman', 'michaelgray@mail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
-    INSERT INTO users (
-    name, email, password)
-    VALUES (
-    'Alejandro Osborne', 'ariaatkinson@outlook.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
+  INSERT INTO users (
+  name, email, password)
+  VALUES (
+  'Devin Sanders', 'tristanjacobs@gmail.com', '1234');
+  INSERT INTO users (
+  name, email, password)
+  VALUES (
+  'Iva Harrison', 'allisonjackson@mail.com', 'abcd');
+  INSERT INTO users (
+  name, email, password)
+  VALUES (
+  'Lloyd Jefferson', 'asherpoole@gmx.com', '1A2B');
+  INSERT INTO users (
+  name, email, password)
+  VALUES (
+  'Dale Coleman', 'michaelgray@mail.com', '5WTDS');
+  INSERT INTO users (
+  name, email, password)
+  VALUES (
+  'Alejandro Osborne', 'ariaatkinson@outlook.com', 'AGFDG');
+  INSERT INTO users (name, email, password)
+  VALUES ('a', 'a@gmail.com', 'ASF236hg.');
+  INSERT INTO users (name, email, password)
+  VALUES ('b', 'b@gmail.com', 'ASFfdh.');
+  INSERT INTO users (name, email, password)
+  VALUES ('c', 'c@gmail.com', 'ASF123.');
--- db/seeds/05_attempts.sql
+++ db/seeds/05_attempts.sql
@@ -1,9 +1,18 @@
-  INSERT INTO attempts (
-  quiz_id, user_id, score)
-  VALUES (
-  1, 3, 5);
-
-  INSERT INTO attempts (
-  quiz_id, user_id, score)
-  VALUES (
-  1, 4, 6);
+  INSERT INTO attempts (quiz_id, user_id, score)
+  VALUES (1, 2, 100);
+  INSERT INTO attempts (quiz_id, user_id, score)
+  VALUES (1, 3, 75);
+  INSERT INTO attempts (quiz_id, user_id, score)
+  VALUES (1, 4, 50);
+  INSERT INTO attempts (quiz_id, user_id, score)
+  VALUES (2, 2, 100);
+  INSERT INTO attempts (quiz_id, user_id, score)
+  VALUES (2, 3, 83);
+  INSERT INTO attempts (quiz_id, user_id, score)
+  VALUES (2, 3, 100);
+  INSERT INTO attempts (quiz_id, user_id, score)
+  VALUES (2, 6, 0);
+  INSERT INTO attempts (quiz_id, user_id, score)
+  VALUES (2, 7, 67);
+  INSERT INTO attempts (quiz_id, user_id, score)
+  VALUES (2, 6, 50);
