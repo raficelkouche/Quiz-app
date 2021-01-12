689dbb6b63662be7e95099b7506654663433cff3
Rafic El Kouche <raficelkouche@hotmail.com>
2021-01-11 21:13:45 -0500

    refactor load more quizzes function

--- public/scripts/homepage.js
+++ public/scripts/homepage.js
@@ -1,16 +1,16 @@
 //contains jQuery/JS for the homepage
 $(() => {
-  let requestCounter = 0;
+  let request_counter = 0;
   $("#load-more").on("click", function () {
-    alert("button clicked");
-    db.getQuizzes(requestCounter)
-      .then(result => {
-        requestCounter++;
-        console.log(result);
-        //create a function that will create a new div with 3 new quizzes
-        result.foreach(elm => {
-          $("#load-more").append(`<div>${elm.title}</div`);
-        });
-      });
+    $.ajax({
+      method:'GET',
+      url: `/api/quizzes/${request_counter}`
+    })
+      .then(results => {
+        request_counter++;
+        results.forEach(elm => {
+          $("#load-more").after(`<div>${elm.title}</div>`);
+        })
+      })
   });
 });
