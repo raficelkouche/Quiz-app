7275e22afaeb7a61ed7781bd8423275d384b4159
Rafic El Kouche <raficelkouche@hotmail.com>
2021-01-11 20:53:44 -0500

    remove DB connection and mount /api route

--- server.js
+++ server.js
@@ -13,15 +13,6 @@ const app            = express();
 const morgan         = require('morgan');
 
 
-
-// PG database client/connection setup
-/*
-const { Pool } = require('pg');
-const dbParams = require('./lib/db.js');
-const db = new Pool(dbParams);
-db.connect();
-*/
-
 // Load the logger first so all (static) HTTP requests are logged to STDOUT
 // 'dev' = Concise output colored by response status for development use.
 //         The :status token will be colored red for server error codes, yellow for client error codes, cyan for redirection codes, and uncolored for all other codes.
@@ -51,8 +42,9 @@ const quizzesRoutes = require("./routes/quizzes");
 const apiRoutes = require("./routes/apiRoutes");
 
 // Mount all resource routes
-app.use("/users", usersRoutes(db));
-app.use("/quizzes", quizzesRoutes(db));
+app.use("/users", usersRoutes());
+app.use("/quizzes", quizzesRoutes());
+app.use("/api", apiRoutes());
 
 // Home page
 app.get("/", (req, res) => {
