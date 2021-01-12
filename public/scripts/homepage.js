346faa2cfcb777c7d3352b8fcaba5ebd416622c5
Rafic El Kouche <raficelkouche@hotmail.com>
2021-01-11 19:36:39 -0500

    work on method to load quizzes

--- public/scripts/homepage.js
+++ public/scripts/homepage.js
@@ -1,11 +1,16 @@
 //contains jQuery/JS for the homepage
 $(() => {
-  $.ajax({
-    method: "GET",
-    url: "/quizzes"
-  }).done((quizzes) => {
-    for (quiz of quizzes) {
-      $("<div>").text(user.name).appendTo($("body"));
-    }
-  });;
+  let requestCounter = 0;
+  $("#load-more").on("click", function () {
+    alert("button clicked");
+    db.getQuizzes(requestCounter)
+      .then(result => {
+        requestCounter++;
+        console.log(result);
+        //create a function that will create a new div with 3 new quizzes
+        result.foreach(elm => {
+          $("#load-more").append(`<div>${elm.title}</div`);
+        });
+      });
+  });
 });
