1d73cba7cba8daa4cd38a4410da238a7363d0578
Wilkie Wai-Kit Wong <feanor4d@gmail.com>
2021-01-13 14:36:27 +0000

    gathering some quiz example as seed for testing purpose. add ~ 30 user, 12 quiz, ~ 70 question and ~ 200 answer, will add attempt next

--- db/seeds/02_quizzes.sql
+++ db/seeds/02_quizzes.sql
@@ -2,12 +2,78 @@
   owner_id, title, description, visibility, photo_url, category)
   VALUES (2, 'Basic Math !', 'Basic addition and subtraction', 'TRUE', 'https://i.imgur.com/Ez7g45q.png', 'Education');
 
-    INSERT INTO quizzes (
+  INSERT INTO quizzes (
   owner_id, title, description, visibility, photo_url, category)
   VALUES (
-  2, 'Fun Math for kids!', 'Basic multiplication', 'TRUE', 'https://i.imgur.com/Um3JMQd.jpeg', 'Education');
+  2, 'Fun Math for kids!', 'Basic multiplication', 'TRUE', 'https://i.imgur.com/UmJMQd.jpeg', 'Education');
 
   INSERT INTO quizzes (
   owner_id, title, description, visibility, photo_url, category)
   VALUES (
   3, 'SCIENCE', 'science for kids!', 'TRUE', 'https://i.imgur.com/D0Iz7.jpeg', 'Education');
+
+  INSERT INTO quizzes ( --quiz 4
+  owner_id, title, description, visibility, photo_url, category)
+  VALUES (
+  15, 'Animal Trivia', 'So you think you know about animal?', 'FALSE', 'https://i.imgur.com/yiNa01J.jpeg', 'Education');
+
+  INSERT INTO quizzes (
+  owner_id, title, description, visibility, photo_url, category)
+  VALUES (
+  8, 'Millennial! (Short)', 'Final Answer?', 'TRUE', 'https://i.imgur.com/9WAb51H.jpg', 'Misc.');
+
+  INSERT INTO quizzes (
+  owner_id, title, description, visibility, photo_url, category)
+  VALUES (
+  7, 'Do you feel Lucky?', '👌👍👌👍👌👍👌👍👌👍good luck go౦ԁ luck👌 have ✔ some good👌👌luck right👌👌there👌👌👌 right✔there ✔✔if i do ƽaү so my self 💯 i say so 💯 thats what im luckin fur right there right there (chorus: ʳᶦᵍʰᵗ ᵗʰᵉʳᵉ) gUUUUUUD💯 👌👌 👌LO0ОଠOOOOOОଠଠOoooᵒᵒᵒᵒᵒᵒᵒᵒᵒ👌 👌👌 👌 💯 👌 💪🏼🙈🙉👌👌Good luck
+', 'TRUE', 'https://i.imgur.com/dA6qCJO.png', 'Misc.');
+
+  INSERT INTO quizzes (
+  owner_id, title, description, visibility, photo_url, category)
+  VALUES (
+  15, 'SCIENCE', 'science for kids!', 'TRUE', '', 'Education');
+
+  INSERT INTO quizzes (
+  owner_id, title, description, visibility, photo_url, category)
+  VALUES (
+  7, 'SCIENCE', 'science for kids!', 'TRUE', '', 'Education');
+
+  INSERT INTO quizzes (
+  owner_id, title, description, visibility, photo_url, category)
+  VALUES (
+  8, 'SCIENCE', 'science for kids!', 'FALSE', '', 'Education');
+
+  INSERT INTO quizzes (
+  owner_id, title, description, visibility, photo_url, category)
+  VALUES (
+  9, 'SCIENCE', 'science for kids!', 'TRUE', '', 'Education');
+
+  INSERT INTO quizzes (
+  owner_id, title, description, visibility, photo_url, category)
+  VALUES (
+  20, 'SCIENCE', 'science for kids!', 'TRUE', '', 'Education');
+
+  INSERT INTO quizzes (
+  owner_id, title, description, visibility, photo_url, category)
+  VALUES (
+  21, 'SCIENCE', 'science for kids!', 'FALSE', '', 'Education');
+
+  INSERT INTO quizzes (
+  owner_id, title, description, visibility, photo_url, category)
+  VALUES (
+  21, 'SCIENCE', 'science for kids!', 'TRUE', '', 'Education');
+
+  INSERT INTO quizzes (
+  owner_id, title, description, visibility, photo_url, category)
+  VALUES (
+  9, 'SCIENCE', 'science for kids!', 'TRUE', '', 'Education');
+
+  INSERT INTO quizzes (
+  owner_id, title, description, visibility, photo_url, category)
+  VALUES (
+  9, 'SCIENCE', 'science for kids!', 'TRUE', '', 'Education');
+
+  INSERT INTO quizzes (
+  owner_id, title, description, visibility, photo_url, category)
+  VALUES (
+  9, 'SCIENCE', 'science for kids!', 'TRUE', '', 'Education');
