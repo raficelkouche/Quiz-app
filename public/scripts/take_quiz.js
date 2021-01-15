f68d97e29a7b64b1f342901ea1345633c3923e13
Rafic El Kouche <raficelkouche@hotmail.com>
2021-01-15 00:09:26 -0500

    style take_quiz page

--- public/scripts/take_quiz.js
+++ public/scripts/take_quiz.js
@@ -1,23 +1,33 @@
 $(() => {
   const questions = $(".quiz-questions").find(".question");
   const numOfQuestions = Number($("#numOfQuestions").text());
+  let i = 0;
 
   $(questions[0]).css("display", "block");
 
-  let i = 1;
   $(".next").on("click", function () {
     if (i < numOfQuestions) {
-      $(questions[i-1]).css("display", "none");
-      $(questions[i]).css("display","block");
+      $(questions[i]).css("display", "none");
+      if (i === numOfQuestions - 1) {
+        $("#submit-quiz-slide").css("display", "flex");
+      }else {
+        $(questions[i+1]).css("display","block");
+      }
       i++;
     }
-    else if (i === numOfQuestions) {
-      $(questions[i-1]).css("display", "none");
-      $("#submit-quiz-slide").css("display", "block");
+  });
+
+  $(".previous").on("click", function () {
+    if (i > 0) {
+      if (i === numOfQuestions) {
+        $("#submit-quiz-slide").css("display", "none");
+      } else {
+        $(questions[i]).css("display", "none");
+      }
+      $(questions[i-1]).css("display", "block");
+      i--;
     }
-    else {
-      i = numOfQuestions;
-    }
+
   })
 })
 
