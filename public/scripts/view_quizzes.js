29c00ef833186599cabd16f179a71311e78d1451
Rafic El Kouche <raficelkouche@hotmail.com>
2021-01-15 15:54:16 -0500

    style view all quizzes

--- public/scripts/view_quizzes.js
+++ public/scripts/view_quizzes.js
@@ -8,9 +8,30 @@ $(() => {
     })
     .then(results => {
       request_counter++;
-      console.log(results);
       results.forEach(elm => {
-        $("#load-more-quizzes").before(`<div>${elm.title}</div>`);
+        $(".all-quizzes .card").last().after(`
+          <div class="card mb-3">
+            <div class="row g-0">
+              <div class="col-12 col-md-6 d-flex">
+                <img src="${elm.photo_url}" alt="">
+              </div>
+              <div class="col-12 col-md-3">
+                <div class="card-body card-content">
+                  <h5 class="card-title">${elm.title}</h5>
+                  <p class="card-text">${elm.descritpion}</p>
+                </div>
+              </div>
+              <div class="col-12 col-md-3">
+                <div class="card-body card-controls">
+                  <a href="/quizzes/${elm.id}" target="_blank" class="btn btn-primary">Take Quiz</a>
+                  <a href="#" target="_blank" class="btn btn-primary">Share Quiz</a>
+                </div>
+              </div>
+            </div>
+        </div>
+
+        `)
+
       })
     })
   });
